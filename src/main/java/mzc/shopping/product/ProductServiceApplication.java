d4b19a22ae3e374daf09506d78a57ce6708e5ac6
amy397 <yjyou98@gmail.com>
2025-12-23 10:58:26 +0900

    image edit

--- src/main/java/mzc/shopping/product/ProductServiceApplication.java
+++ src/main/java/mzc/shopping/product/ProductServiceApplication.java
@@ -1,9 +1,12 @@
 package mzc.shopping.product;
 
+import mzc.shopping.product.config.FileConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(FileConfig.class)  // 이 줄 추가!
 public class ProductServiceApplication {
 
     public static void main(String[] args) {
